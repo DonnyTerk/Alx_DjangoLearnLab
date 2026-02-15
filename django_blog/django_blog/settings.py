3c4d5065a1c2836d593ba257db9459b18a645def
DonnyTer <ui.uxter@gmail.com>
2026-02-15 11:53:37 +0100

    Initial commit

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 LOGIN_REDIRECT_URL = "profile"
 LOGOUT_REDIRECT_URL = "login"
