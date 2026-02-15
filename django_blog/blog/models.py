b4065d87448dd19e4043f155d99ddec39de999ee
DonnyTer <ui.uxter@gmail.com>
2026-02-15 09:31:51 +0100

    blog html templates

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 
 # Create your models here.
 from django.db import models
@@ -17,3 +16,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+
+    def __str__(self):
+        return self.user.username
