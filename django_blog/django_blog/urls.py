ed67f039de0b7a12a80da00a0d0a9e07efb24a7b
DonnyTer <ui.uxter@gmail.com>
2026-02-17 16:54:31 +0100

    initiation and authentication of login, register && profile

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,24 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from blog import views   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
 ]
-
-from django.urls import path
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
     path("profile/", views.profile, name="profile"),
-]
-
-from django.urls import path, include
-
-urlpatterns = [
-    path("", include("blog.urls")),
 ]
