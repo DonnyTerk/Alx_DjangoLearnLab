0cf118550fc7a110746e2d37aa48953ccee1ae68
DonnyTer <ui.uxter@gmail.com>
2026-02-18 17:14:24 +0100

    initiating tempaltes

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -10,5 +8,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
-# Create your models here.
