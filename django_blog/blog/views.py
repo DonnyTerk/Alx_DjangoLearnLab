25a293a4c18a0d46882474645bfc5cfe06d26295
DonnyTer <ui.uxter@gmail.com>
2026-02-19 23:40:37 +0100

    implementing a comment system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,16 +2,20 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
-
 from django.shortcuts import render
-
-def index(request):
-    return render(request, 'blog/index.html')
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .models import Post, Comment
+from .forms import CommentForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
+
+
+def index(request):
+    return render(request, 'blog/index.html')
 
 def register(request):
     if request.method == 'POST':
@@ -73,3 +77,58 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+
+# --- ADD A COMMENT ---
+@login_required                         # only logged-in users can comment
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)  # don't save to DB yet
+            comment.post = post                # attach the post
+            comment.author = request.user      # attach the logged-in user
+            comment.save()                     # NOW save to DB
+            return redirect('post-detail', pk=post.id)  # go back to the post
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+
+
+# --- EDIT A COMMENT ---
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    # Only the comment's author can edit it
+    if comment.author != request.user:
+        return HttpResponseForbidden("You can't edit someone else's comment.")
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)  # pre-fill with existing data
+        if form.is_valid():
+            form.save()
+            return redirect('post-detail', pk=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+
+
+# --- DELETE A COMMENT ---
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    # Only the comment's author can delete it
+    if comment.author != request.user:
+        return HttpResponseForbidden("You can't delete someone else's comment.")
+
+    if request.method == 'POST':
+        post_id = comment.post.id
+        comment.delete()
+        return redirect('post-detail', pk=post_id)
+
+    return render(request, 'blog/delete_comment.html', {'comment': comment})
