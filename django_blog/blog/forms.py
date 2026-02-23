6cfc6cf89ccbfa62df58c06ec056d216357e4285
DonnyTer <ui.uxter@gmail.com>
2026-02-23 09:26:17 +0100

    update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,23 +1,35 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from .models import Comment, Post
 from django.contrib.auth.models import User
-from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
+from taggit.forms import TagWidget                    
+
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']        
+        widgets = {
+            'tags': TagWidget()                       
+        }
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content']   # author and post are set automatically in the view
+        fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={
                 'rows': 4,
-                'placeholder': 'Write your comment here...'
+                'placeholder': 'comment...'
             })
         }
         labels = {
